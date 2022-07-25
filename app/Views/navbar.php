6cfe52f24b9807e85435270cb81a12c8e170ec3b
Dthariq347 <65454556+Dthariq347@users.noreply.github.com>
2022-07-25 20:11:48 +0700

    mengubah navbar

--- app/Views/navbar.php
+++ app/Views/navbar.php
@@ -10,6 +10,7 @@
                 <li class="nav-item"><a class="nav-link" href="#services">Our Values</a></li>
                 <li class="nav-item"><a class="nav-link" href="#portfolio">Our Product</a></li>
                 <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
             </ul>
         </div>
     </div>
